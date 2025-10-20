f6ab3f1ccaddc400a57118cf2829ff098699d364
OsicKwon <osic.kwon@gmail.com>
2025-10-20 14:26:33 -0400

    Update class_abstractmethod.py

--- @abstractmethod/class_abstractmethod.py
+++ @abstractmethod/class_abstractmethod.py
@@ -1,25 +1,35 @@
-
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import override
 
-
 class StudentBase(ABC):
-    """Abstract base class for Student."""
+    """Abstract base class for Student"""
 
+    @abstractmethod
     def study(self):
-        """Study method."""
         pass
+ 
+    @abstractmethod
+    def go_to_school(self):
+        ...
 
 
 class Student(StudentBase):
-    """Subclass implementing abstract methods."""
+    """ Subclass implementing abstract methods """
 
     @override
-    def studie(self):
-        """Wrongly overridden method."""
-        print("studying")
+    def study(self):
+        print('studying')
+
+    @override
+    def go_to_school(self):
+        print('going to school')
 
 
-osic = Student()
-osic.study()  # print: studying
-osic.go_to_school()  # print: going to school
+def main():
+    osic = Student()
+    osic.study()  # print: studying
+    osic.go_to_school()  # print: going to school
+
+
+if __name__ == "__main__":
+    main()
